b967f0298a45d17f50b6fd4ae432c3236015888f
AayushMehta0924 <aayushmehta0924@gmail.com>
2025-01-11 16:21:31 -0600

    JSON style format update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -24,8 +24,13 @@ const About = () => {
                 className="w-full lg:w-1/2"
             >
                 <div className="flex justify-center lg:justify-start">
-                    <p className="my-2 max-w-xl py-6">{ABOUT_TEXT}</p>
+                    <pre className="my-2 max-w-xl py-6 bg-neutral-100 p-4 rounded-md shadow-lg">
+                        {ABOUT_TEXT}
+                    </pre>
                 </div>
+                {/* <div className="flex justify-center lg:justify-start">
+                    <p className="my-2 max-w-xl py-6">{ABOUT_TEXT}</p>
+                </div> */}
             </motion.div>
         </div>
     </div>
