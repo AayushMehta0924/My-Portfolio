4f97fd090076b57823759b9d6a3121b62de0102c
AayushMehta0924 <aayushmehta0924@gmail.com>
2025-01-11 17:42:24 -0600

    JSON style text format update complete

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,66 +5,71 @@ import { motion } from "framer-motion";
 const About = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
-        <h2 className="my-20 text-center text-4xl">About <span className="text-neutral-500">Me</span></h2>
+        <h2 className="my-20 text-center text-4xl">Hello <span className="text-neutral-500">World</span></h2>
         <div className="flex flex-wrap">
-            <motion.div 
+            {/* Image Section */}
+            <motion.div
                 whileInView={{ opacity: 1, x: 0 }}
                 initial={{ opacity: 0, x: -100 }}
                 transition={{ duration: 0.5 }}
-                className="w-full lg:w-1/2 lg:p-8"
+                className="w-full lg:w-1/2 lg:p-8 flex items-center justify-center"
             >
-                <div className="flex items-center justify-center">
-                    <img className="rounded-2xl" src={aboutImg} alt="about" />
-                </div>
+                <img className="rounded-2xl" src={aboutImg} alt="about" />
             </motion.div>
-            <motion.div 
+
+            {/* Text Section */}
+            <motion.div
                 whileInView={{ opacity: 1, x: 0 }}
                 initial={{ opacity: 0, x: 100 }}
                 transition={{ duration: 0.5 }}
-                className="w-full lg:w-1/2"
+                className="w-full lg:w-1/2 relative lg:top-[-10px]" // Adjust this value as needed
             >
-                <div className="flex justify-center lg:justify-start">
-                    <pre className="my-2 max-w-xl py-6 bg-transparent p-4 rounded-md shadow-lg">
-                        <code>
-                            {"{"}
-                            <br />
-                            <span className="text-red-500">"I am a"</span>: <span className="text-green-500">"Passionate full stack developer with 5+ years of experience"</span>,
-                            <br />
-                            <span className="text-yellow-500">"Currently"</span>: <span className="text-green-500">"Exploring cutting-edge technologies and open to new opportunities"</span>,
-                            <br />
-                            <span className="text-red-500">"Previously at"</span>: [
-                            <br />
-                            &nbsp;&nbsp;<span className="text-green-500">"Nividata Consultancy"</span>,
-                            <br />
-                            &nbsp;&nbsp;<span className="text-green-500">"Bosky Group"</span>,
-                            <br />
-                            &nbsp;&nbsp;<span className="text-green-500">"Twowaits Technologies"</span>
-                            <br />
-                            ],
-                            <br />
-                            <span className="text-yellow-500">"Studied at"</span>: <span className="text-green-500">"University of Texas at Dallas"</span>,
-                            <br />
-                            <span className="text-red-500">"I enjoy"</span>: [
-                            <br />
-                            &nbsp;&nbsp;<span className="text-green-500">"Exploring AI/ML advancements"</span>,
-                            <br />
-                            &nbsp;&nbsp;<span className="text-green-500">"Contributing to open-source projects"</span>,
-                            <br />
-                            &nbsp;&nbsp;<span className="text-green-500">"Playing basketball"</span>,
-                            <br />
-                            &nbsp;&nbsp;<span className="text-green-500">"Learning new technologies"</span>
-                            <br />
-                            ],
-                            <br />
-                            <span className="text-yellow-500">"Contact me at"</span>: <span className="text-green-500">"aayushmehta0924@gmail.com"</span>
-                            <br />
-                            {"}"}
-                        </code>
-                    </pre>
-                </div>
-                {/* <div className="flex justify-center lg:justify-start">
-                    <p className="my-2 max-w-xl py-6">{ABOUT_TEXT}</p>
-                </div> */}
+                <pre className="my-2 max-w-xl py-6 bg-transparent p-4 rounded-md shadow-lg">
+                    <code>
+                        {"{"}
+                        <br />
+                        &nbsp;&nbsp;<span className="text-red-500">"I am a"</span>: {"{"}
+                        <br />
+                        &nbsp;&nbsp;&nbsp;&nbsp;<span className="text-yellow-500">"hometown"</span>: <span className="text-green-500">"Ahmedabad, India"</span>,
+                        <br />
+                        &nbsp;&nbsp;&nbsp;&nbsp;<span className="text-yellow-500">"current_base"</span>: <span className="text-green-500">"Dallas, Texas"</span>,
+                        <br />
+                        &nbsp;&nbsp;&nbsp;&nbsp;<span className="text-yellow-500">"languages"</span>: <span className="text-green-500">"English, Hindi, Gujarati"</span>,
+                        <br />
+                        &nbsp;&nbsp;{"},"}
+                        <br />
+                        &nbsp;&nbsp;<span className="text-red-500">"Currently"</span>: <span className="text-green-500">"Exploring cutting-edge technologies"</span>,
+                        <br />
+                        &nbsp;&nbsp;<span className="text-red-500">"Education"</span>: [
+                        <br />
+                        &nbsp;&nbsp;&nbsp;&nbsp;{"{"}<br />
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span className="text-yellow-500">"Masters in Computer Science"</span>: {"{"}
+                        <br />
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span className="text-yellow-500">"Arizona State University"</span>: <span className="text-green-500">"GPA: 3.81/4"</span>
+                        <br />
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{"}"},
+                        <br />
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span className="text-yellow-500">"Bachelors in Computer Science and Engineering"</span>: {"{"}
+                        <br />
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span className="text-yellow-500">"S.R.M. University"</span>: <span className="text-green-500">"CGPA: 9.38/10"</span>
+                        <br />
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{"}"}
+                        <br />
+                        &nbsp;&nbsp;&nbsp;&nbsp;{"}"},
+                        <br />
+                        &nbsp;&nbsp;],
+                        <br />
+                        &nbsp;&nbsp;<span className="text-red-500">"Hobbies"</span>: [
+                        <br />
+                        &nbsp;&nbsp;&nbsp;&nbsp;<span className="text-green-500">"Football (the real one)"</span>,
+                        <br />
+                        &nbsp;&nbsp;&nbsp;&nbsp;<span className="text-green-500">"Piano"</span>,
+                        <br />
+                        &nbsp;&nbsp;]
+                        <br />
+                        {"}"}
+                    </code>
+                </pre>
             </motion.div>
         </div>
     </div>
